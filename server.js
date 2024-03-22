36bf1d121f4c2eed8b6cde61fa894deb407bd334
pistorello <leopmatos0605@outlook.com>
2024-03-22 13:04:43 -0300

    first commit pistorello

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const crypto = require('crypto');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-const port = 80;
+const port = 89;
 
 // Conexão com o MongoDB
-const uri = 'mongodb://localhost:27017';
+const uri = 'mongodb://26.138.203.133:27017';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Middlewares
